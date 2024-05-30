4700c09911a95de0407b346e094457ca580828f1
Fahmi Dzulqarnain <fdz.fhz1302@gmail.com>
2024-05-30 11:12:31 +0800

    Add Fortify Authentication

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,20 +3,20 @@
     <form class="form-inline mr-auto">
         <ul class="navbar-nav mr-3">
             <li><a href="#"
-                    data-toggle="sidebar"
-                    class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
+                   data-toggle="sidebar"
+                   class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
             <li><a href="#"
-                    data-toggle="search"
-                    class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li>
+                   data-toggle="search"
+                   class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li>
         </ul>
         <div class="search-element">
             <input class="form-control"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-                data-width="250">
+                   type="search"
+                   placeholder="Search"
+                   aria-label="Search"
+                   data-width="250">
             <button class="btn"
-                type="submit"><i class="fas fa-search"></i></button>
+                    type="submit"><i class="fas fa-search"></i></button>
             <div class="search-backdrop"></div>
             <div class="search-result">
                 <div class="search-header">
@@ -25,17 +25,17 @@
                 <div class="search-item">
                     <a href="#">How to hack NASA using CSS</a>
                     <a href="#"
-                        class="search-close"><i class="fas fa-times"></i></a>
+                       class="search-close"><i class="fas fa-times"></i></a>
                 </div>
                 <div class="search-item">
                     <a href="#">Kodinger.com</a>
                     <a href="#"
-                        class="search-close"><i class="fas fa-times"></i></a>
+                       class="search-close"><i class="fas fa-times"></i></a>
                 </div>
                 <div class="search-item">
                     <a href="#">#Stisla</a>
                     <a href="#"
-                        class="search-close"><i class="fas fa-times"></i></a>
+                       class="search-close"><i class="fas fa-times"></i></a>
                 </div>
                 <div class="search-header">
                     Result
@@ -43,27 +43,27 @@
                 <div class="search-item">
                     <a href="#">
                         <img class="mr-3 rounded"
-                            width="30"
-                            src="{{ asset('img/products/product-3-50.png') }}"
-                            alt="product">
+                             width="30"
+                             src="{{ asset('img/products/product-3-50.png') }}"
+                             alt="product">
                         oPhone S9 Limited Edition
                     </a>
                 </div>
                 <div class="search-item">
                     <a href="#">
                         <img class="mr-3 rounded"
-                            width="30"
-                            src="{{ asset('img/products/product-2-50.png') }}"
-                            alt="product">
+                             width="30"
+                             src="{{ asset('img/products/product-2-50.png') }}"
+                             alt="product">
                         Drone X2 New Gen-7
                     </a>
                 </div>
                 <div class="search-item">
                     <a href="#">
                         <img class="mr-3 rounded"
-                            width="30"
-                            src="{{ asset('img/products/product-1-50.png') }}"
-                            alt="product">
+                             width="30"
+                             src="{{ asset('img/products/product-1-50.png') }}"
+                             alt="product">
                         Headphone Blitz
                     </a>
                 </div>
@@ -91,8 +91,8 @@
     </form>
     <ul class="navbar-nav navbar-right">
         <li class="dropdown dropdown-list-toggle"><a href="#"
-                data-toggle="dropdown"
-                class="nav-link nav-link-lg message-toggle beep"><i class="far fa-envelope"></i></a>
+               data-toggle="dropdown"
+               class="nav-link nav-link-lg message-toggle beep"><i class="far fa-envelope"></i></a>
             <div class="dropdown-menu dropdown-list dropdown-menu-right">
                 <div class="dropdown-header">Messages
                     <div class="float-right">
@@ -101,11 +101,11 @@
                 </div>
                 <div class="dropdown-list-content dropdown-list-message">
                     <a href="#"
-                        class="dropdown-item dropdown-item-unread">
+                       class="dropdown-item dropdown-item-unread">
                         <div class="dropdown-item-avatar">
                             <img alt="image"
-                                src="{{ asset('img/avatar/avatar-1.png') }}"
-                                class="rounded-circle">
+                                 src="{{ asset('img/avatar/avatar-1.png') }}"
+                                 class="rounded-circle">
                             <div class="is-online"></div>
                         </div>
                         <div class="dropdown-item-desc">
@@ -115,11 +115,11 @@
                         </div>
                     </a>
                     <a href="#"
-                        class="dropdown-item dropdown-item-unread">
+                       class="dropdown-item dropdown-item-unread">
                         <div class="dropdown-item-avatar">
                             <img alt="image"
-                                src="{{ asset('img/avatar/avatar-2.png') }}"
-                                class="rounded-circle">
+                                 src="{{ asset('img/avatar/avatar-2.png') }}"
+                                 class="rounded-circle">
                         </div>
                         <div class="dropdown-item-desc">
                             <b>Dedik Sugiharto</b>
@@ -128,11 +128,11 @@
                         </div>
                     </a>
                     <a href="#"
-                        class="dropdown-item dropdown-item-unread">
+                       class="dropdown-item dropdown-item-unread">
                         <div class="dropdown-item-avatar">
                             <img alt="image"
-                                src="{{ asset('img/avatar/avatar-3.png') }}"
-                                class="rounded-circle">
+                                 src="{{ asset('img/avatar/avatar-3.png') }}"
+                                 class="rounded-circle">
                             <div class="is-online"></div>
                         </div>
                         <div class="dropdown-item-desc">
@@ -142,11 +142,11 @@
                         </div>
                     </a>
                     <a href="#"
-                        class="dropdown-item">
+                       class="dropdown-item">
                         <div class="dropdown-item-avatar">
                             <img alt="image"
-                                src="{{ asset('img/avatar/avatar-4.png') }}"
-                                class="rounded-circle">
+                                 src="{{ asset('img/avatar/avatar-4.png') }}"
+                                 class="rounded-circle">
                         </div>
                         <div class="dropdown-item-desc">
                             <b>Ardian Rahardiansyah</b>
@@ -155,11 +155,11 @@
                         </div>
                     </a>
                     <a href="#"
-                        class="dropdown-item">
+                       class="dropdown-item">
                         <div class="dropdown-item-avatar">
                             <img alt="image"
-                                src="{{ asset('img/avatar/avatar-5.png') }}"
-                                class="rounded-circle">
+                                 src="{{ asset('img/avatar/avatar-5.png') }}"
+                                 class="rounded-circle">
                         </div>
                         <div class="dropdown-item-desc">
                             <b>Alfa Zulkarnain</b>
@@ -174,8 +174,8 @@
             </div>
         </li>
         <li class="dropdown dropdown-list-toggle"><a href="#"
-                data-toggle="dropdown"
-                class="nav-link notification-toggle nav-link-lg beep"><i class="far fa-bell"></i></a>
+               data-toggle="dropdown"
+               class="nav-link notification-toggle nav-link-lg beep"><i class="far fa-bell"></i></a>
             <div class="dropdown-menu dropdown-list dropdown-menu-right">
                 <div class="dropdown-header">Notifications
                     <div class="float-right">
@@ -184,7 +184,7 @@
                 </div>
                 <div class="dropdown-list-content dropdown-list-icons">
                     <a href="#"
-                        class="dropdown-item dropdown-item-unread">
+                       class="dropdown-item dropdown-item-unread">
                         <div class="dropdown-item-icon bg-primary text-white">
                             <i class="fas fa-code"></i>
                         </div>
@@ -194,7 +194,7 @@
                         </div>
                     </a>
                     <a href="#"
-                        class="dropdown-item">
+                       class="dropdown-item">
                         <div class="dropdown-item-icon bg-info text-white">
                             <i class="far fa-user"></i>
                         </div>
@@ -204,7 +204,7 @@
                         </div>
                     </a>
                     <a href="#"
-                        class="dropdown-item">
+                       class="dropdown-item">
                         <div class="dropdown-item-icon bg-success text-white">
                             <i class="fas fa-check"></i>
                         </div>
@@ -214,7 +214,7 @@
                         </div>
                     </a>
                     <a href="#"
-                        class="dropdown-item">
+                       class="dropdown-item">
                         <div class="dropdown-item-icon bg-danger text-white">
                             <i class="fas fa-exclamation-triangle"></i>
                         </div>
@@ -224,7 +224,7 @@
                         </div>
                     </a>
                     <a href="#"
-                        class="dropdown-item">
+                       class="dropdown-item">
                         <div class="dropdown-item-icon bg-info text-white">
                             <i class="fas fa-bell"></i>
                         </div>
@@ -240,32 +240,39 @@
             </div>
         </li>
         <li class="dropdown"><a href="#"
-                data-toggle="dropdown"
-                class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+               data-toggle="dropdown"
+               class="nav-link dropdown-toggle nav-link-lg nav-link-user">
                 <img alt="image"
-                    src="{{ asset('img/avatar/avatar-1.png') }}"
-                    class="rounded-circle mr-1">
-                <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div>
+                     src="{{ asset('img/avatar/avatar-1.png') }}"
+                     class="rounded-circle mr-1">
+                <div class="d-sm-none d-lg-inline-block">Hi, {{ auth()->user()->name }}</div>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
                 <div class="dropdown-title">Logged in 5 min ago</div>
                 <a href="features-profile.html"
-                    class="dropdown-item has-icon">
+                   class="dropdown-item has-icon">
                     <i class="far fa-user"></i> Profile
                 </a>
                 <a href="features-activities.html"
-                    class="dropdown-item has-icon">
+                   class="dropdown-item has-icon">
                     <i class="fas fa-bolt"></i> Activities
                 </a>
                 <a href="features-settings.html"
-                    class="dropdown-item has-icon">
+                   class="dropdown-item has-icon">
                     <i class="fas fa-cog"></i> Settings
                 </a>
                 <div class="dropdown-divider"></div>
                 <a href="#"
-                    class="dropdown-item has-icon text-danger">
+                   class="dropdown-item has-icon text-danger"
+                   onclick="event.preventDefault(); document.getElementById('logout-form').submit()">
                     <i class="fas fa-sign-out-alt"></i> Logout
                 </a>
+                <form id="logout-form"
+                      action="{{ route('logout') }}"
+                      method="POST"
+                      class="d-none">
+                    @csrf
+                </form>
             </div>
         </li>
     </ul>
